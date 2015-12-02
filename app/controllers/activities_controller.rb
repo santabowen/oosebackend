0c36bb1c913c42a126503fc3d6e70e4da0dc05ac
Xi Yang <clcblueljw@gmail.com>
2015-12-02 17:15:00 -0500

    try to fix order by startime phrase 1

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -89,6 +89,7 @@ class ActivitiesController < ApplicationController
   			 			 longitude > #{@min_lng} AND 
   			 			 latitude < #{@max_lat} AND 
   			 			 latitude > #{@min_lat}
+  			 ORDER BY a.startTime
   			")
   		
 	    rtnacts = []
